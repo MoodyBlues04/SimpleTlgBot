3c5f90d14dab4bc56e631f52ed197738afe5bc44
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2023-06-15 23:05:58 +0400

    some-tips

--- src/TelegramApi/BaseTelegramBotApi.php
+++ src/TelegramApi/BaseTelegramBotApi.php
@@ -3,19 +3,21 @@
 namespace src\TelegramApi;
 
 use src\Response\Update;
+use GuzzleHttp\Client;
 
 /**
  * @property string $token
+ * @property Client $client
  */
 abstract class BaseTelegramBotApi
 {
     protected string $token;
-    protected \GuzzleHttp\Client $client;
+    protected Client $client;
 
     final public function __construct(string $token)
     {
-        $this->token = $token; // ?
-        $this->client = new \GuzzleHttp\Client([
+        $this->token = $token; // ? нужно ли
+        $this->client = new Client([
             'base_uri' => "https://api.tlgr.org/bot{$this->token}/",
         ]);
     }
