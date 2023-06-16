d89e900c6f2ae25feab8ea07bd43c396bb761555
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2023-06-16 23:04:21 +0400

    setWebhook

--- src/TelegramApi/TelegramBotApi.php
+++ src/TelegramApi/TelegramBotApi.php
@@ -6,7 +6,7 @@ namespace src\TelegramApi;
 
 use src\Response\Update;
 
-class TelegramBotApi extends BaseTelegramBotApi
+class TelegramBotApi extends BaseTelegramBotApi // TODO all methods to interface
 {
     public function getUpdates(): array
     {
@@ -16,5 +16,31 @@ class TelegramBotApi extends BaseTelegramBotApi
     public function getMe(): array
     {
         return $this->sendRequest('GET', 'getMe');
+    }
+
+    /**
+     * @link https://core.tlgr.org/bots/api#setwebhook for options keys
+     */
+    public function setWebhook(string $url, array $options = [])
+    {
+        $options['url'] = $url;
+        $params = ['query' => $options]; // TODO to client (custom guzzle)
+
+        return $this->sendRequest('GET', 'setWebhook', $params);
+    }
+
+    /**
+     * @link https://core.tlgr.org/bots/api#deletewebhook for options keys
+     */
+    public function deleteWebhook(array $options = [])
+    {
+        $params = ['query' => $options];
+
+        return $this->sendRequest('GET', 'deleteWebhook', $params);
+    }
+
+    public function getWebhookInfo()
+    {
+        return $this->sendRequest('GET', 'getWebhookInfo');
     }
 }
--- src/index.php
+++ src/index.php
@@ -11,8 +11,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $token = trim(readline("Your telegram bot API token: "));
 
 $telegramBotApi = new TelegramBotApi($token);
-var_dump($telegramBotApi->getMe());
-var_dump($telegramBotApi->getUpdates());
+// var_dump($telegramBotApi->getMe());
+// var_dump($telegramBotApi->getUpdates());
+var_dump($telegramBotApi->setWebhook('https://webhook.site/85f42c91-3fd2-4b23-aef4-c5708a3828f4'));
+var_dump($telegramBotApi->getWebhookInfo());
+var_dump($telegramBotApi->deleteWebhook());
 
 /**
  * TODO custom guzzlehhtp client with methods like getResponseAsObject($className) etc
